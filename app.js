76cf6ad54e7249105130601c35f6a8ca863c036c
Mohamed1442 <mohamedmagdy7428@gmail.com>
2023-08-01 19:43:29 +0300

    third commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(routePath, "public")));
 
 app.use((req, res, next) => {
-  User.findById("64bf41b7d9226748c79ecb5e")
+  User.findById("64c9350e7914d2c64d8f1d94")
     .then((user) => {
       req.user = user;
       next();
